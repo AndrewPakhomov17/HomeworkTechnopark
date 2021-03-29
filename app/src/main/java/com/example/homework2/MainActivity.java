eea8b5ab4bd8f5c909ace106dfcde0c79ec307bc
Andrew Pakhomov <klenovyi.sirop@mail.ru>
2021-03-29 11:08:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements OnNumberClickListener {
+
+    public final static String RECYCLERVIEW_TAG = "RECYCLERVIEW_TAG";
+    public final static String NUMBER_TAG = "NUMBER_TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRecyclerViewFragment();
+    }
+
+    @Override
+    public void onClick(int value) {
+        setNumberFragment(value);
+    }
+
+
+    private void setRecyclerViewFragment() {
+        if (getSupportFragmentManager().findFragmentByTag(RECYCLERVIEW_TAG) == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, new RecyclerViewFragment(), RECYCLERVIEW_TAG)
+                    .commit();
+        }
+    }
+
+    private void setNumberFragment(int value) {
+        if (getSupportFragmentManager().findFragmentByTag(NUMBER_TAG) == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new NumberFragment(value), NUMBER_TAG)
+                    .addToBackStack(NUMBER_TAG)
+                    .commit();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/homework2/OnNumberClickListener.java
@@ -0,0 +1,6 @@
+package com.example.homework2;
+
+
+public interface OnNumberClickListener {
+    void onClick(int value);
+}
